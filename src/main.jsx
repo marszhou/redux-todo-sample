d4b88845f6e8e20c9f54312d93b15504aca982f8
mars <mars@huoban.com>
2017-08-15 00:12:27 +0800

    Make sublime display the jsx code more kindly

---
+++ src/main.jsx
@@ -0,0 +1,223 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, combineReducers} from 'redux'
+document.title = 'Redux Full Sample'
+
+const todo = (state={}, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      }
+    case 'TOGGLE_TODO':
+      return {
+        ...state,
+        completed: state.id === action.id ?
+          !state.completed : state.completed
+      }
+    default:
+      return state
+  }
+}
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        todo(undefined, action)
+      ]
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action))
+    default:
+      return state
+  }
+}
+
+const visibilityFilter = (
+  state = 'SHOW_ALL',
+  action
+) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter
+    default:
+      return state
+  }
+}
+
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+})
+
+const store = createStore(todoApp)
+
+const getVisibleTodos = (
+  todos,
+  filter
+) => {
+  switch (filter) {
+    case 'SHOW_ALL':
+      return todos
+    case 'SHOW_ACTIVE':
+      return todos.filter(t => !t.completed)
+    case 'SHOW_COMPLETED':
+      return todos.filter(t => t.completed)
+  }
+}
+
+const FilterLink = ({
+  filter,
+  currentFilter,
+  onClick,
+  children
+}) => {
+  if (filter === currentFilter) {
+    return (
+      <span>
+        {children}
+      </span>
+    )
+  }
+  return (
+    <a href='#'
+       onClick={ e => {
+        e.preventDefault()
+        onClick(filter)
+       }}
+    >
+      {children}
+    </a>
+  )
+}
+
+const Footer = ({
+  visibilityFilter,
+  onFilterClick
+}) => {
+  return (
+    <p>
+      Show:
+      {' '}
+      <FilterLink
+        filter='SHOW_ALL'
+        currentFilter={visibilityFilter}
+        onClick={onFilterClick}
+      >All</FilterLink>
+      {', '}
+      <FilterLink
+        filter='SHOW_ACTIVE'
+        currentFilter={visibilityFilter}
+        onClick={onFilterClick}
+      >Active</FilterLink>
+      {', '}
+      <FilterLink
+        filter='SHOW_COMPLETED'
+        currentFilter={visibilityFilter}
+        onClick={onFilterClick}
+      >Completed</FilterLink>
+    </p>
+  )
+}
+
+const Todo = ({
+  onClick,
+  completed,
+  text
+}) => (
+  <li
+    onClick={onClick}
+    style={{
+      textDecoration: completed
+        ? 'line-through' : 'none'
+    }}
+>{text}</li>
+)
+
+const TodoList = ({
+  todos,
+  onTodoClick
+}) => (
+  <ul>
+    {
+      todos.map(todo =>
+        (<Todo
+          key={todo.id}
+          {...todo}
+          onClick={() => onTodoClick(todo.id)}
+         />)
+      )
+    }
+  </ul>
+)
+
+const AddTodo = ({
+  onAddClick
+}) => {
+  let input
+
+  return (
+    <form
+      onSubmit={ e => {
+        e.preventDefault()
+        let text = input.value.trim()
+        if (text.length === 0) return
+        onAddClick(text)
+        input.value = ''
+        input.focus()
+      }}
+    >
+      <input type='text' ref={node => input = node}/>
+      <button>Add Todo</button>
+    </form>
+  )
+}
+
+
+
+let nextTodoId = 0
+const TodoApp = ({todos, visibilityFilter}) => {
+  let visibleTodos = getVisibleTodos(todos, visibilityFilter)
+
+  return (<div>
+    <AddTodo
+      onAddClick={text =>
+        store.dispatch({
+          type: 'ADD_TODO',
+          text,
+          id: nextTodoId++
+        })
+      }/>
+    <TodoList
+      todos={visibleTodos}
+      onTodoClick={ id =>
+        store.dispatch({
+          type: 'TOGGLE_TODO',
+          id
+        })
+      }
+    />
+    <Footer
+      visibilityFilter={visibilityFilter}
+      onFilterClick={filter =>
+        store.dispatch({
+          type: 'SET_VISIBILITY_FILTER',
+          filter
+        })
+      }
+    />
+  </div>)
+}
+
+const render = () => {
+  ReactDOM.render(
+    (<TodoApp {...store.getState()}/>),
+    document.getElementById('root')
+  )
+}
+
+store.subscribe(render)
+render()
