d018b5242568d116d886a587812ebe7b7daab463
mars <mars@huoban.com>
2017-08-15 00:09:57 +0800

    Extracting Presentational Components (AddTodo, Footer, FilterLink)

--- src/index.js
+++ src/index.js
@@ -77,23 +77,54 @@ const getVisibleTodos = (
 const FilterLink = ({
   filter,
   currentFilter,
+  onClick,
   children
 }) => {
   if (filter === currentFilter) {
-    return (<span>{children}</span>)
+    return (
+      <span>
+        {children}
+      </span>
+    )
   }
   return (
     <a href='#'
        onClick={ e => {
         e.preventDefault()
-        store.dispatch({
-          type: 'SET_VISIBILITY_FILTER',
-          filter
-        })
+        onClick(filter)
        }}
     >
       {children}
     </a>
+  )
+}
+
+const Footer = ({
+  visibilityFilter,
+  onFilterClick
+}) => {
+  return (
+    <p>
+      Show:
+      {' '}
+      <FilterLink
+        filter='SHOW_ALL'
+        currentFilter={visibilityFilter}
+        onClick={onFilterClick}
+      >All</FilterLink>
+      {', '}
+      <FilterLink
+        filter='SHOW_ACTIVE'
+        currentFilter={visibilityFilter}
+        onClick={onFilterClick}
+      >Active</FilterLink>
+      {', '}
+      <FilterLink
+        filter='SHOW_COMPLETED'
+        currentFilter={visibilityFilter}
+        onClick={onFilterClick}
+      >Completed</FilterLink>
+    </p>
   )
 }
 
@@ -128,22 +159,18 @@ const TodoList = ({
   </ul>
 )
 
-let nextTodoId = 0
-const TodoApp = ({todos, visibilityFilter}) => {
+const AddTodo = ({
+  onAddClick
+}) => {
   let input
-  let visibleTodos = getVisibleTodos(todos, visibilityFilter)
 
-  return (<div>
+  return (
     <form
       onSubmit={ e => {
         e.preventDefault()
         let text = input.value.trim()
         if (text.length === 0) return
-        store.dispatch({
-          type: 'ADD_TODO',
-          id: nextTodoId++,
-          text
-        })
+        onAddClick(text)
         input.value = ''
         input.focus()
       }}
@@ -151,6 +178,24 @@ const TodoApp = ({todos, visibilityFilter}) => {
       <input type='text' ref={node => input = node}/>
       <button>Add Todo</button>
     </form>
+  )
+}
+
+
+
+let nextTodoId = 0
+const TodoApp = ({todos, visibilityFilter}) => {
+  let visibleTodos = getVisibleTodos(todos, visibilityFilter)
+
+  return (<div>
+    <AddTodo
+      onAddClick={text =>
+        store.dispatch({
+          type: 'ADD_TODO',
+          text,
+          id: nextTodoId++
+        })
+      }/>
     <TodoList
       todos={visibleTodos}
       onTodoClick={ id =>
@@ -160,24 +205,15 @@ const TodoApp = ({todos, visibilityFilter}) => {
         })
       }
     />
-    <p>
-      Show:
-      {' '}
-      <FilterLink
-        filter='SHOW_ALL'
-        currentFilter={visibilityFilter}
-      >All</FilterLink>
-      {', '}
-      <FilterLink
-        filter='SHOW_ACTIVE'
-        currentFilter={visibilityFilter}
-      >Active</FilterLink>
-      {', '}
-      <FilterLink
-        filter='SHOW_COMPLETED'
-        currentFilter={visibilityFilter}
-      >Completed</FilterLink>
-    </p>
+    <Footer
+      visibilityFilter={visibilityFilter}
+      onFilterClick={filter =>
+        store.dispatch({
+          type: 'SET_VISIBILITY_FILTER',
+          filter
+        })
+      }
+    />
   </div>)
 }
 
