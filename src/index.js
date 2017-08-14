eb8077bb89d685ac1604397051cf031efd331537
mars <mars@huoban.com>
2017-08-14 23:06:28 +0800

    Implementing combineReducers() from Scratch

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker';
 // import expect from 'expect'
-import {createStore, combineReducers} from 'redux'
+import {createStore} from 'redux'
 // import deepFreeze from 'deep-freeze'
 registerServiceWorker();
 
@@ -52,23 +52,20 @@ const visibilityFilter = (
       return state
   }
 }
-
+const combineReducers = (reducers) => {
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce(
+      (nextState, key) => {
+        nextState[key] = reducers[key](state[key], action)
+        return nextState
+      }, {}
+    )
+  }
+}
 const todoApp = combineReducers({
   todos,
   visibilityFilter
 })
-// const todoApp = (state = {}, action ) => {
-//   return {
-//     todos: todos(
-//       state.todos,
-//       action
-//     ),
-//     visibilityFilter: visibilityFilter(
-//       state.visibilityFilter,
-//       action
-//     )
-//   }
-// }
 
 const store = createStore(todoApp)
 
