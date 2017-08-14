989c437161d6198cb71fad1a61d12722d9299f3a
mars <mars@huoban.com>
2017-08-14 23:28:07 +0800

    React Todo List Example (Toggling a Todo)

--- src/index.js
+++ src/index.js
@@ -84,7 +84,17 @@ const TodoApp = ({todos}) => {
     <ul>
       {
         todos.map(todo => (
-          <li key={todo.id}>{todo.text}</li>
+          <li key={todo.id}
+              onClick={
+                () => {
+                  store.dispatch({
+                    type: 'TOGGLE_TODO',
+                    id: todo.id
+                  })
+                }
+              }
+              style={{textDecoration: todo.completed ? 'line-through' : 'none'}}
+          >{todo.text}</li>
         ))
       }
     </ul>
