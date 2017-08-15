4d38835fd0974f9af1ab8f2e5856e5567b1653cb
mars <mars@huoban.com>
2017-08-15 11:26:39 +0800

    Generating Containers with connect() from React Redux (VisibleTodoList)

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {createStore, combineReducers} from 'redux'
-import {Provider} from 'react-redux'
+import {Provider, connect} from 'react-redux'
 
 const todo = (state={}, action) => {
   switch (action.type) {
@@ -176,42 +176,28 @@ const TodoList = ({
   </ul>
 )
 
-class VisibleTodoList extends React.Component {
-  componentWillMount() {
-    const {store} = this.context
-    this.unsubscribe = store.subscribe(() => this.forceUpdate())
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe()
-  }
-
-  render() {
-    const props = this.props
-    const {store} = this.context
-    const state = store.getState()
-
-    return (
-      <TodoList
-        todos={
-          getVisibleTodos(
-            state.todos,
-            state.visibilityFilter
-          )
-        }
-        onTodoClick={id =>
-          store.dispatch({
-            type: 'TOGGLE_TODO',
-            id
-          })
-        }
-      />
+const mapStateToProps = (state) => {
+  return {
+    todos: getVisibleTodos(
+      state.todos,
+      state.visibilityFilter
     )
   }
 }
-VisibleTodoList.contextTypes = {
-  store: React.PropTypes.object
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTodoClick: id => {
+      dispatch({
+        type: 'TOGGLE_TODO',
+        id
+      })
+    }
+  }
 }
+const VisibleTodoList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList)
 
 const AddTodo = ({}, {store}) => {
   let input
