0b3cf8cbfaa4b615443ba7c10a257f72ebbd6b76
mars <mars@huoban.com>
2017-08-17 14:44:23 +0800

    Normalizing the State Shape

---
+++ src/reducers/todo.js
@@ -0,0 +1,20 @@
+const todo = (state={}, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      }
+    case 'TOGGLE_TODO':
+      return {
+        ...state,
+        completed: state.id === action.id ?
+          !state.completed : state.completed
+      }
+    default:
+      return state
+  }
+}
+
+export default todo
