7ea9576933226e501aea0924be849c13b690b6d5
mars <mars@huoban.com>
2017-08-17 14:21:57 +0800

    Merge Todo into Todos

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,4 +1,21 @@
-import todo from './todo'
+const todo = (state={}, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      }
+    case 'TOGGLE_TODO':
+      return {
+        ...state,
+        completed: state.id === action.id ?
+          !state.completed : state.completed
+      }
+    default:
+      return state
+  }
+}
 
 export default (state = [], action) => {
   switch (action.type) {
